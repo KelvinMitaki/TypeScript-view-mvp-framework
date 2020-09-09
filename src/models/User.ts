388895e621b7e949ee79469557ef07c7ea259cc8
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-09 12:28:59 +0300

    wired up attributes to user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
+import { Attributes } from "./Attributes";
 export interface UserProps {
   name?: string;
   age?: number;
@@ -9,6 +10,10 @@ export interface UserProps {
 export type Callback = () => void;
 const rootUrl = "http://localhost:3000/users";
 export class User {
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
   public event: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 }
